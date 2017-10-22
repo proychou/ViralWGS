9f9e435964a55b2505448798798eed892bc5d6a3
Pavitra Roychoudhury <pavitra.padmanabhan@gmail.com>
2017-10-22 14:44:33 -0700

    bug fix
     in make_ref_from_assembly()

--- wgs_functions.R
+++ wgs_functions.R
@@ -48,8 +48,9 @@ make_ref_from_assembly<-function(bamfname,reffname){
 																			shift=mcols(gal)$pos-1,Lpadding.letter='N',Rpadding.letter='N');
 		
 		#Make a consensus matrix and get a consensus sequence from the aligned scaffolds
+		#to do: also '-'s should be filled, what if assembly is guessing them wrong?
 		cm<-consensusMatrix(qseq_on_ref_aligned,as.prob=T,shift=0)[c('A','C','G','T','N','-'),];
-		cm['N',]<-0;
+		cm[c('N','-'),]<-0;
 		cm<-apply(cm,2,function(x)if(all(x==0))return(x) else return(x/sum(x)));
 		cm['N',colSums(cm)==0]<-1;
 		con_seq<-DNAStringSet(consensusString(cm,threshold=0.25));
@@ -164,49 +165,6 @@ generate_consensus<-function(bamfname){
     con_seq<-DNAStringSet(paste0(tmp_str,collapse='')); 
     names(con_seq)<-sub('.bam','_consensus',basename(bamfname));
     rm(tmp_str);
-    
-    #Look for insertions in bam cigar string--this is not practical with mapped reads
-    #Takes too much time. Need to figure out a way to merge duplicates. 
-    #In any case, most insertions should have been captured in assembly so should be in consensus
-    # cigs_ref<-cigarRangesAlongReferenceSpace(cigar(gal),with.ops=F,ops='I',
-    # 																				 reduce.ranges=T,drop.empty.ranges=F,
-    # 																				 pos=mcols(gal)$pos);
-    # cigs_query<-cigarRangesAlongQuerySpace(cigar(gal),ops='I',with.ops=F,
-    # 																			 reduce.ranges=T,drop.empty.ranges=F);
-    # all_ins<-mclapply(c(1:length(cigs_query)),function(i)
-    # 	extractAt(mcols(gal)$seq[i],cigs_query[[i]])[[1]]);
-    
-    # #Merge all insertions
-    # all_ins_merged<-do.call('rbind',mclapply(c(1:length(cigs_ref)),function(i)
-    # 	return(data.frame(
-    # 		start_ref=start(cigs_ref[[i]]),end_ref=end(cigs_ref[[i]]),
-    # 		start_query=start(cigs_query[[i]]),end_query=end(cigs_query[[i]]),
-    # 		ins_seq=all_ins[[i]],width_ins=width(all_ins[[i]]))),
-    # 	mc.cores=ncores));
-    # all_ins_merged<-all_ins_merged[order(all_ins_merged$end_ref),];
-    # # write.csv(all_ins_merged,'./testing/all_ins.csv',row.names=F);
-    # 
-    # #TO DO: Check for overlapping insertions and figure out how to deal with them
-    # if(any(table(all_ins_merged$start_ref)>1)) browser()
-    # 
-    # #Now the beauty part of inserting the strings back in
-    # #Split ref seq by the insert positions
-    # new_strs<-DNAStringSet(rep('',nrow(all_ins_merged)))
-    # for(i in 1:nrow(all_ins_merged)){
-    # 	if(i==1){
-    # 		new_strs[i]<-paste0(extractAt(con_seq,IRanges(start=1,end=all_ins_merged$end_ref[i]))[[1]],
-    # 												all_ins_merged$ins_seq[i]);
-    # 	}else if(i==nrow(all_ins_merged)){
-    # 		new_strs[i]<-paste0(all_ins_merged$ins_seq[i],
-    # 												extractAt(con_seq,IRanges(start=all_ins_merged$start_ref[i],
-    # 																									end=width(con_seq)))[[1]])
-    # 	}else{
-    # 		new_strs[i]<-paste0(extractAt(con_seq,IRanges(start=all_ins_merged$start_ref[i-1],
-    # 																									end=all_ins_merged$end_ref[i]))[[1]],
-    # 												all_ins_merged$ins_seq[i]);
-    # 	}
-    # }
-    # temp_str<-paste0(as.character(new_strs),collapse='');
     
     #Remove gaps to get final sequence
     # con_seq_final<-DNAStringSet(gsub('-','',temp_str));
